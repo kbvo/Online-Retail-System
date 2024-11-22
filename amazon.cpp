d3c25c86a5e71d8df4d3cdce8321c3a507f85c26
Kevin Vo <149135851+kbvo@users.noreply.github.com>
2024-11-21 20:14:41 -0800

    Update amazon.cpp

--- amazon.cpp
+++ amazon.cpp
@@ -26,27 +26,18 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    /****************
-     * Declare derived DataStore object here replacing
-     *  DataStore type to derived type
-     ****************/
     MyDataStore ds;
 
-
-
-    // Instantiate the individual section and product parsers we want
     ProductSectionParser* productSectionParser = new ProductSectionParser;
     productSectionParser->addProductParser(new ProductBookParser);
     productSectionParser->addProductParser(new ProductClothingParser);
     productSectionParser->addProductParser(new ProductMovieParser);
     UserSectionParser* userSectionParser = new UserSectionParser;
 
-    // Instantiate the parser
     DBParser parser;
     parser.addSectionParser("products", productSectionParser);
     parser.addSectionParser("users", userSectionParser);
 
-    // Now parse the database to populate the DataStore
     if( parser.parse(argv[1], ds) ) {
         cerr << "Error parsing!" << endl;
         return 1;
